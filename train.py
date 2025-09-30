80098fbf087ee8b47680941a99ead5a09691d66d
dadudu13 <dulin.wang@hotmail.com>
2025-09-30 14:09:05 -0500

    version1

---
+++ train.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+drug_df = pd.read_csv("Data/drug200.csv")
+drug_df = drug_df.sample(frac=1) # Shuffle all rows randomly
+drug_df.head(3)
+
+from sklearn.model_selection import train_test_split
+
+X = drug_df.drop("Drug", axis=1).values
+y = drug_df.Drug.values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=125
+)
+
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
+cat_col = [1,2,3]
+num_col = [0,4]
+
+transform = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col), #Scales features to have mean=0 and std=1 
+        # Before: Age=[20, 60, 40] (range 40), Sodium=[0.8, 0.9, 0.7] (range 0.2)
+        # After:  Both features have similar scales, preventing age from dominating
+    ]
+)
+pipe = Pipeline(
+    steps=[
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
+    ]
+)
+pipe.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score, f1_score
+
+predictions = pipe.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+f1 = f1_score(y_test, predictions, average="macro")
+
+
+with open("Results/metrics.txt", "w") as outfile:
+    outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
+disp.plot()
+plt.savefig("Results/model_results.png", dpi=120)
+
+import skops.io as sio
+
+sio.dump(pipe, "Model/drug_pipeline.skops")
+
+# Try to get untrusted types using the file parameter
+untrusted_types = sio.get_untrusted_types(file="Model/drug_pipeline.skops")
+print(f"Untrusted types: {untrusted_types}")
+
+# Load with those types
+sio.load("Model/drug_pipeline.skops", trusted=untrusted_types)
