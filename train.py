8067e6108b2b163aee70a3264aa11f9090059084
dadudu13 <dulin.wang@hotmail.com>
2025-10-01 03:26:05 +0000

    Update with new results

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 drug_df = pd.read_csv("Data/drug200.csv")
-drug_df = drug_df.sample(frac=1) # Shuffle all rows randomly
+drug_df = drug_df.sample(frac=1)  # Shuffle all rows randomly
 drug_df.head(3)
 
 from sklearn.model_selection import train_test_split
@@ -19,14 +19,18 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
-cat_col = [1,2,3]
-num_col = [0,4]
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 
 transform = ColumnTransformer(
     [
         ("encoder", OrdinalEncoder(), cat_col),
         ("num_imputer", SimpleImputer(strategy="median"), num_col),
-        ("num_scaler", StandardScaler(), num_col), #Scales features to have mean=0 and std=1 
+        (
+            "num_scaler",
+            StandardScaler(),
+            num_col,
+        ),  # Scales features to have mean=0 and std=1
         # Before: Age=[20, 60, 40] (range 40), Sodium=[0.8, 0.9, 0.7] (range 0.2)
         # After:  Both features have similar scales, preventing age from dominating
     ]
